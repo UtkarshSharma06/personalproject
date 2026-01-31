9482bad62a145b2cca30273c70e6e877f020abc3
Utkarsh Sharma <utkarsh@example.com>
2026-01-31 06:04:19 +0530

    fix: enable session detection for web login

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -14,6 +14,6 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     storage: CapacitorStorage,
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: false, // Important for mobile to avoid hash conflict
+    detectSessionInUrl: true,
   }
 });
